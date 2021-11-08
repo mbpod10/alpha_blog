fd9daa62a30ef9f2363d135559069c23229a38fa
M. Brock Podgurski <mbpod10@gmail.com>
2021-11-08 13:20:55 -0500

    user edit in browswer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:edit, :update] 
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
   end
 
+  def edit  
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User ID #{params[:id].to_s} Was Updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-     flash[:notice] = "Welcome To The Alpha Blog, #{@user.username} Successfully Created"
+     flash[:notice] = "Welcome To The Alpha Blog, #{@user.username} Successfully Updated"
      redirect_to articles_path
     else
       render 'new'
@@ -16,6 +33,10 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
