2ff906b1e95d22a4d0125712f91ded86ccef456f
M. Brock Podgurski <mbpod10@gmail.com>
2021-11-08 15:55:13 -0500

    create user list and user show routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only:[:edit, :update] 
+  before_action :set_user, only:[:edit, :update, :show] 
+
+  def show
+    @articles = @user.articles
+  end
 
   def index
     @users = User.all
